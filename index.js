954c7ce62c9301e8b0062eaa74ff247fcf8acb86
mesmatyi <mesicsmatyas@gmail.com>
2021-10-03 23:13:04 +0200

    Add mapbox changes

--- index.js
+++ index.js
@@ -1,54 +1,127 @@
 let MarkerArray = []
 
-function initMap() {
-  var options = {
-    center: { lat: 47.68501, lng: 16.59049 },
-    zoom: 14,
-  };
 
-  const myLatlng = { lat: 47.68501, lng: 16.59049 };
+function zoom_scale(accuracy)
+{
+  var scale = accuracy / 250;
+  scale = Math.round(scale);
 
-  map = new google.maps.Map(document.getElementById("map"), options);
+  if(scale > 4) scale = 4;
 
-  // Create the initial InfoWindow.
-  let infoWindow = new google.maps.InfoWindow({
-    content: "Start!",
-    position: myLatlng,
-  });
-  infoWindow.open(map);
-  // Configure the click listener.
-  map.addListener("click", (mapsMouseEvent) => {
-    // Close the current InfoWindow.
-    infoWindow.close();
-    // Create a new InfoWindow.
-    infoWindow = new google.maps.InfoWindow({
-      position: mapsMouseEvent.latLng,
-    });
-    MarkerArray.push(mapsMouseEvent.latLng.toJSON());
-    infoWindow.setContent(
-      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-    );
-    infoWindow.open(map);
-  });
+  added_zoom = 4 - scale;
 
-  google.maps.event.addListener(map, "click", (event) => {
-    //Add marker
-    addMarker({ location: event.latLng });
-  });
-
-  function addMarker(property) {
-    const marker = new google.maps.Marker({
-      position: property.location,
-      map: map,
-      icon: property.imageIcon,
-    });
-  }
+  return 9 + added_zoom;
 }
-//geocode();
-//getLocationForm
-var locationForm = document.getElementById("location-form");
-//Listen for submit
-locationForm.addEventListener("submit", geocode);
+
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+  console.log(pos.coords);
+
+  initMap(pos.coords);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+  let coords = null;
+  initMap(coords);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+function initMap(coords)
+{
+  let lng = 0;
+  let lat = 0;
+  let zoom = 1;
+  if(coords != null)
+  {
+    lng = coords['longitude'];
+    lat = coords['latitude'];
+    zoom =  zoom_scale(coords['accuracy']);
+  }
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWVzaWNzbWF0eWkiLCJhIjoiY2swM2pndWttMGE0ZjNtcDU0Yjc1ejF0YiJ9.QTpGLoEnNwVb6lR1xab2NQ';
+  map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+  center: [lng, lat], // starting position [lng, lat]
+  zoom: zoom // starting zoom
+  });
+  
+  map.on('click', (e) => {
+    MarkerArray.push(e.lngLat.wrap());
+    new mapboxgl.Marker({ color: 'black'})
+      .setLngLat([e.lngLat.wrap()['lng'],e.lngLat.wrap()['lat']])
+      .addTo(map);
+  });
+
+  const coordinatesGeocoder = function (query) {
+    // Match anything which looks like
+    // decimal degrees coordinate pair.
+    const matches = query.match(
+    /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+    );
+    if (!matches) {
+    return null;
+    }
+    
+    function coordinateFeature(lng, lat) {
+    return {
+    center: [lng, lat],
+    geometry: {
+    type: 'Point',
+    coordinates: [lng, lat]
+    },
+    place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+    place_type: ['coordinate'],
+    properties: {},
+    type: 'Feature'
+    };
+    }
+    
+    const coord1 = Number(matches[1]);
+    const coord2 = Number(matches[2]);
+    const geocodes = [];
+    
+    if (coord1 < -90 || coord1 > 90) {
+    // must be lng, lat
+    geocodes.push(coordinateFeature(coord1, coord2));
+    }
+    
+    if (coord2 < -90 || coord2 > 90) {
+    // must be lat, lng
+    geocodes.push(coordinateFeature(coord2, coord1));
+    }
+    
+    if (geocodes.length === 0) {
+    // else could be either lng, lat or lat, lng
+    geocodes.push(coordinateFeature(coord1, coord2));
+    geocodes.push(coordinateFeature(coord2, coord1));
+    }
+    
+    return geocodes;
+    };
+    
+    // Add the control to the map.
+      map.addControl(
+      new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      localGeocoder: coordinatesGeocoder,
+      zoom: 4,
+      placeholder: 'Search for places',
+      mapboxgl: mapboxgl,
+      reverseGeocode: true
+      })
+    );
+    }
 
 function getMatrixFromResponse(responsJson,cost_type)
 {
@@ -76,7 +149,6 @@ function getMatrixFromResponse(responsJson,cost_type)
 
 function plan_route()
 {
-
   const plan_profile = 'walking';
   
   const cost_type = ''; // Null is duration, or distance
@@ -114,63 +186,4 @@ function plan_route()
   .catch(error=>{console.log(error)})
   
 
-}
-
-function geocode(e) 
-{
-  //Prevent actual submit
-  e.preventDefault();
-
-  var location = document.getElementById("start-input").value;
-
-  axios
-    .get("https://maps.googleapis.com/maps/api/geocode/json", {
-      params: {
-        address: location,
-        key: "AIzaSyDaomzBPGjAnjdsjUln-S2Di9YFCESsHHo",
-      },
-    })
-    .then(function (response) {
-      //Log full response
-      console.log(response);
-
-      //Formated Address
-      var formattedAddress = response.data.results[0].formatted_address;
-      var formattedAddressOutput = `
-      <ul class"list-group>
-      <li class="list-group-item">${formattedAddress}</li>
-      </ul>
-      `;
-      //Address Componets
-      var addressComponents = response.data.results[0].address_components;
-      var addressComponentsOutput = '<ul class="list-group">';
-      for (var i = 0; i < addressComponents.length; i++) {
-        addressComponentsOutput += `
-        <li class="list-group-item"><stong>${addressComponents[i].types[0]}</strong>:${addressComponents[i].long_name}</li>
-        `;
-      }
-      addressComponentsOutput += "</ul>";
-
-      //Geometry
-      var lat = response.data.results[0].geometry.location.lat;
-      var lng = response.data.results[0].geometry.location.lng;
-      var geometryOutput = `
-      <ul class"list-group>
-      <li class="list-group-item"><stong>Latitude</stong>:${lat}</li>
-      <li class="list-group-item"><stong>Longitude</stong>:${lng}</li>
-      </ul>
-      `;
-
-      //Output to app
-      document.getElementById(
-        "formatted-address"
-      ).innerHTML = formattedAddressOutput;
-      document.getElementById(
-        "address-components"
-      ).innerHTML = addressComponentsOutput;
-      document.getElementById("geometry").innerHTML = geometryOutput;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 }
