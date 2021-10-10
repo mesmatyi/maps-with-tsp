164f61b5d55801341f7d5c68ff7a2f0b21570ec9
mesmatyi <mesicsmatyas@gmail.com>
2021-10-10 09:50:07 +0200

    Add correct download option

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ function onFailed(error) {
 
 function download_gpx(filename, text) {
   var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute(encodeURIComponent(text));
   element.setAttribute('download', filename);
 
   element.style.display = 'none';
