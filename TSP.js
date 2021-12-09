c50e181bb31ec198f052a4311abf35e1753f5386
tibortakacs15 <tibortakacs94@gmail.com>
2021-12-09 14:22:54 +0100

    delete comments

--- TSP.js
+++ TSP.js
@@ -188,56 +188,11 @@ function minKoltseg(matrix, s_matrix, vec){
 
 }
 
-/*
-let matrix = [["W", 2, 11, 10, 8, 7, 6, 5],
-              [6, "W", 1, 8, 8, 4, 6, 7],
-              [5, 12, "W", 11, 8, 12, 3, 11],
-              [11, 9, 10, "W", 1, 9, 8, 10],
-              [11, 11, 9, 4, "W", 2, 10, 9],
-              [12, 8, 5, 2, 11, "W", 11, 9],
-              [10, 11, 12, 10, 9, 12, "W", 3],
-              [7, 10, 10, 10, 6, 3, 1, "W"]]
-*/
-
 let s_matrix = JSON.parse(JSON.stringify(matrix));
 let vec = JSON.parse(JSON.stringify(matrix[0]));
 let m = [];
 m = minKoltseg(matrix, s_matrix, vec);
 console.log(m);
-
-// console.log(s_matrix);
-// console.log(m);
-// let reszkorut_N_A = [];
-// let reszkorut_N_I = [];
-// let reszkorut_F_I = [];
-
-// let celfuggveny_N_A = 0.0;
-// let celfuggveny_N_I = 0.0;
-// let celfuggveny_F_I = 0.0;
-
-// console.log("TSP_Nearest_Insertion");
-// vec = JSON.parse(JSON.stringify(matrix[0]));
-// reszkorut_N_I = TSP_Nearest_Insertion(s_matrix, vec);
-// console.log(reszkorut_N_I);
-// celfuggveny_N_I = celfuggvenyKiszamitasa(matrix, reszkorut_N_I);
-// console.log("Celfuggveny:");
-// console.log(celfuggveny_N_I);
-
-
-// console.log("TSP_Farthest_Insertion");
-// vec = JSON.parse(JSON.stringify(matrix[0]));
-// reszkorut_F_I = TSP_Farthest_Insertion(s_matrix, vec);
-// console.log(reszkorut_F_I);
-// celfuggveny_F_I = celfuggvenyKiszamitasa(matrix, reszkorut_F_I);
-// console.log("Celfuggveny:");
-// console.log(celfuggveny_F_I);
-
-// console.log("TSP_Nearest_Addition");
-// vec = JSON.parse(JSON.stringify(matrix[0]));
-// reszkorut_N_A = TSP_Nearest_Addition(s_matrix, vec);
-// console.log(reszkorut_N_A);
-// celfuggveny_N_A = celfuggvenyKiszamitasa(matrix, reszkorut_N_A);
-// console.log("Celfuggveny:");let s_matrix = JSON.parse(JSON.stringify(matrix));
 
 
 
